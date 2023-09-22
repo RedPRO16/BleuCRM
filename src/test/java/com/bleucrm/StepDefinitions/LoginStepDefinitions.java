cc1f29e01819d8160eb02d23c4080a020064c99a
Redpro <nesho1616@gmail.com>
2023-09-22 20:52:34 +0300

    login added2

--- src/test/java/com/bleucrm/StepDefinitions/LoginStepDefinitions.java
+++ src/test/java/com/bleucrm/StepDefinitions/LoginStepDefinitions.java
@@ -40,4 +40,90 @@ public class LoginStepDefinitions {
         Assert.assertTrue(loginPage.ActivityStream.isDisplayed());
     }
 
+    @When("user enters the Marketing username")
+    public void userEntersTheMarketingUsername() {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("Marketing.username"));
+    }
+
+    @And("user enters the Marketing password")
+    public void userEntersTheMarketingPassword() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("Marketing.password"));
+    }
+
+    @When("user enters the Helpdesk username")
+    public void userEntersTheHelpdeskUsername() {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("Helpdesk.username"));
+    }
+
+    @And("user enters the Helpdesk password")
+    public void userEntersTheHelpdeskPassword() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("Helpdesk.password"));
+    }
+
+    @When("user enters the invalid username")
+    public void userEntersTheInvalidUsername() {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("invalid.username"));
+    }
+
+    @And("user enters the valid password")
+    public void userEntersTheValidPassword() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("Marketing.password"));
+    }
+
+    @Then("user can see incorrect username or password text")
+    public void userCanSeeIncorrectUsernameOrPasswordText() {
+        String actualErrorMessage = loginPage.incorrectLoginOrPassword.getText();
+        String expectedErrorMessage = ConfigurationReader.getProperty("expected.error.message.without.username.or.password");
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage);
+
+    }
+
+    @When("user enters the valid username")
+    public void userEntersTheValidUsername() {
+        loginPage.userName.sendKeys(ConfigurationReader.getProperty("Marketing.username"));
+    }
+
+    @And("user enters the invalid password")
+    public void userEntersTheInvalidPassword() {
+        loginPage.password.sendKeys(ConfigurationReader.getProperty("invalid.password"));
+    }
+
+    @And("user do not enter the password")
+    public void userDoNotEnterThePassword() {
+
+    }
+
+    @Then("user can see Please fill out this field message")
+    public void userCanSeePleaseFillOutThisFieldMessage() {
+        String actualErrorMessage = loginPage.incorrectLoginOrPassword.getText();
+        Assert.assertEquals(ConfigurationReader.getProperty("expected.error.message.2"), actualErrorMessage);
+
+    }
+
+    @When("user checks Forgot password link is visible")
+    public void userChecksForgotPasswordLinkIsVisible() {
+        Assert.assertTrue(loginPage.linkForgotYourPassword.isDisplayed());
+    }
+
+    @Then("user clicks on the link and verifies the link is clickable")
+    public void userClicksOnTheLinkAndVerifiesTheLinkIsClickable() {
+        loginPage.linkForgotYourPassword.click();
+    }
+
+    @When("user checks Remember Me button is visible")
+    public void userChecksRememberMeButtonIsVisible() {
+        Assert.assertTrue(loginPage.rememberMe.isDisplayed());
+    }
+
+    @Then("user clicks on the link and verifies the button is clickable")
+    public void userClicksOnTheLinkAndVerifiesTheButtonIsClickable() {
+        loginPage.rememberMe.click();
+        Assert.assertTrue(loginPage.rememberMe.isSelected());
+    }
+
+    @Then("user can see the password is in bullet sign")
+    public void userCanSeeThePasswordIsInBulletSign() {
+        String passwordInputType = loginPage.password.getAttribute("type");
+        Assert.assertEquals("password",passwordInputType);
+    }
 }
